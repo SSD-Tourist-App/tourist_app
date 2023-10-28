334d75437951abc2f56f772c1887569b4be2f040
tituz175 <tobititus01@gmail.com>
2023-10-28 16:41:31 -0600

    app_library removed.

--- python_program/review/app_library.py
+++
@@ -1,5 +0,0 @@
-from applicationfuntions import web_scrape
-
-content = web_scrape.wikiScraper("https://www.tripadvisor.com/Attraction_Review-g47088-d7209454-Reviews-Plaza_Antiques-Las_Vegas_New_Mexico.html")
-with open("content.txt", "w") as file:
-        file.write(content)
